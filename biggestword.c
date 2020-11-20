760a7414007fd47131085891508b89f9c494aff4
Stefano Palumbo <stefano.palumbo001@studenti.uniparthenope.it>
2020-11-20 19:53:04 +0100

    Aggiunto parole in A****E

--- biggestword.c
+++ biggestword.c
@@ -2,27 +2,26 @@
 #include <string.h>
 int main()
 {
-    char testo[] = "All previous releases are listed on GitHub, with release notes, and are available for download from Amazon S3. They are handy for testing, but otherwise we recommend using the latest stable release. Some older GitHub release pages include the same binaries for downloading. But we have discontinued including binaries as assets on newer release pages because of the large number of files for each release";
+    char testo[] = "All previous releases are listed on GitHub, with release notes, and are available for download from Amazon S3. They are handy for testing, but otherwise we recommend using the latest stable release. Some older GitHub release pages andare asserire include the same binaries for downloading. But we have discontinued including binaries as assets on newer release pages because of the large number of files for each release";
     char spazio[] = " ";
     char maxparola[100] = "";
     char *token;
+    int counter = 0;
     token = strtok(testo, spazio);
     int maxlen = 0, index;
     while (token != NULL)
     {
-        char *address1 = testo;
-        char *address2 = &(*token);
-        int calcolo = (address1 - address2) * -1;
-        printf("%p %p %s %i\n", &(*token), &testo, token, strlen(token));
+        printf("%p %p %s %i\n", token, testo, token, strlen(token));
         if (maxlen < strlen(token))
         {
             maxlen = strlen(token);
             strcpy(maxparola, token);
-            index = calcolo;
+            index = (token - testo);
         }
+        if(token[0] == *"a" && token[strlen(token) - 1] == *"e"){counter++;}
 
         token = strtok(NULL, spazio);
     }
 
-    printf("%i %i %s", maxlen, index, maxparola);
+    printf("%i %i %s %i", maxlen, index, maxparola, counter);
 }
